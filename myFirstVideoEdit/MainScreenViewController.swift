e31276a017a33282e432abf0d5f7b2299a057738
Guido Grassel <guido@Guidos-MacBook-Pro.local>
2014-09-22 22:21:54 +0300

    merging video seems to work, but saving to right location TBD

--- myFirstVideoEdit/MainScreenViewController.swift
+++ myFirstVideoEdit/MainScreenViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-import MediaPlayer;
+import AVFoundation;
+import CoreMedia;
 
 class MainScreenViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -16,11 +17,12 @@ class MainScreenViewController: UIViewController,  UIImagePickerControllerDelega
     @IBOutlet weak var movieThumbImage3: UIImageView!
     @IBOutlet weak var movieThumbImage4: UIImageView!
     
-    var movieThumbsImageViews : [UIImageView] = nil;
-    var movieThumbsImages : [UIImage] =
+    var movieThumbsImageViews : [UIImageView] = [UIImageView]();
+    var movieThumbsImages : [UIImage] = [UIImage]();
+    var movieUrls : [NSURL] = [NSURL]();
     
-    var editedMediaUrl : NSURL!;
-    var originalMediaUrl : NSURL!;
+    var movieCount = 0;
+    let movieCountMax = 4;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +30,8 @@ class MainScreenViewController: UIViewController,  UIImagePickerControllerDelega
         movieThumbsImageViews = [
             movieThumbImage1!, movieThumbImage2!, movieThumbImage3!, movieThumbImage4!
         ];
-        // Do any additional setup after loading the view, typically from a nib.
+        movieThumbsImages = [UIImage](count: 4, repeatedValue: UIImage());
+        movieUrls = [NSURL](count: 4, repeatedValue: NSURL());
     }
 
  
@@ -69,10 +72,10 @@ class MainScreenViewController: UIViewController,  UIImagePickerControllerDelega
 
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        self.originalMediaUrl = info[UIImagePickerControllerMediaURL] as? NSURL;
-        self.editedMediaUrl = info[UIImagePickerControllerReferenceURL] as? NSURL;
+        var originalMediaUrl = info[UIImagePickerControllerMediaURL] as? NSURL;
+        var editedMediaUrl = info[UIImagePickerControllerReferenceURL] as? NSURL;
         println("editedMediaUrl: \(editedMediaUrl), originalMediaUrl: \(originalMediaUrl)");
-        picker.dismissViewControllerAnimated(true, completion: nil)
+        picker.dismissViewControllerAnimated(true, completion: { var p = editedMediaUrl; self.addMovie(editedMediaUrl!) } )
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
@@ -80,13 +83,97 @@ class MainScreenViewController: UIViewController,  UIImagePickerControllerDelega
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    func addMovie(editedMediaUrl : NSURL) {
+        if movieCount < movieCountMax {
+            movieUrls[movieCount] = editedMediaUrl;
+            movieThumbsImages[movieCount] = generateThumb(editedMediaUrl);
+            movieThumbsImageViews[movieCount].image  =  movieThumbsImages[movieCount];
+            movieCount++;
+        }
+    }
+    
     func generateThumb(movieUrl : NSURL) -> UIImage {
-        var player = MPMoviePlayerController(contentURL: movieUrl);
-        var image = player.thumbnailImageAtTime(1.0, timeOption:MPMovieTimeOptionNearestKeyFrame);
-        
+        // http://stackoverflow.com/questions/19105721/thumbnailimageattime-now-deprecated-whats-the-alternative
+        var asset : AVURLAsset = AVURLAsset (URL: movieUrl, options: nil);
+        var generator : AVAssetImageGenerator = AVAssetImageGenerator(asset: asset);
+        generator.appliesPreferredTrackTransform = true;
+        var time : CMTime = CMTimeMake(1,2);
+        var oneRef : CGImageRef = generator.copyCGImageAtTime(time, actualTime: nil, error: nil);
+        var image : UIImage = UIImage(CGImage: oneRef);
         return image;
     }
     
+    @IBAction func exportMovieSelected(sender: AnyObject) {
+        exportVideo3(applicationDocumentsDirectory());
+    }
+    
+    func exportVideo3(outputPath:String) {
+        // http://stackoverflow.com/questions/25403315/ios-swift-merge-videos-using-avfoundation
+        if (movieCount < 2) {
+            println("at leats two movies needed for merging");
+            return;
+        }
+        
+        // the final composition, consisting of a video and an audio track.
+        var composition = AVMutableComposition()
+        let trackVideo:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        let trackAudio:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID())
+        var insertTime = kCMTimeZero
+        var index = 0;
+        
+        for index in 0 ... movieCount-1 {
+            let moviePathUrl = movieUrls[index];
+            let sourceAsset = AVURLAsset(URL: moviePathUrl, options: nil)
+            
+            let tracks = sourceAsset.tracksWithMediaType(AVMediaTypeVideo)
+            let audios = sourceAsset.tracksWithMediaType(AVMediaTypeAudio)
+            
+            if tracks.count > 0 {
+                // append the first video and the first audio track to trackVideo / trackAudio
+                let assetTrack : AVAssetTrack = tracks[0] as AVAssetTrack
+                trackVideo.insertTimeRange(CMTimeRangeMake(kCMTimeZero,sourceAsset.duration), ofTrack: assetTrack, atTime: insertTime, error: nil)
+                
+                if audios.count > 0 {
+                    let assetTrackAudio:AVAssetTrack = audios[0] as AVAssetTrack
+                    trackAudio.insertTimeRange(CMTimeRangeMake(kCMTimeZero,sourceAsset.duration), ofTrack: assetTrackAudio, atTime: insertTime, error: nil)
+                }
+                insertTime = CMTimeAdd(insertTime, sourceAsset.duration)
+            }
+        }
+        
+        
+        let completeMovie = outputPath.stringByAppendingPathComponent("movie.mov")
+        let completeMovieUrl = NSURL(fileURLWithPath: completeMovie)
+        
+        var exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality)
+        exporter.outputURL = completeMovieUrl
+        exporter.outputFileType = AVFileTypeMPEG4   //AVFileTypeQuickTimeMovie
+        exporter.exportAsynchronouslyWithCompletionHandler({
+            switch exporter.status
+                {
+                case  AVAssetExportSessionStatus.Failed:
+                    println("failed \(exporter.error)")
+                case AVAssetExportSessionStatus.Cancelled:
+                    println("cancelled \(exporter.error)")
+                default:
+                    println("complete")
+                }
+            })
+    }
+  
+    func applicationDocumentsDirectory() -> String {
+        
+        var documentsDirectory : String?
+        var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true);
+        
+        if paths.count > 0 {
+            if let pathString = paths[0] as? NSString {
+                documentsDirectory = pathString
+            }
+        }
+        
+        return documentsDirectory!;
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
