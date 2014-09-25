d33d8d4c04d482f2327c7172f4039e13e13be577
Guido Grassel <Guido.grassel@nokia.com>
2014-09-25 19:31:52 +0300

    added the custom video composition and OpenGL renderer classes from the APL example

---
+++ myFirstVideoEdit/myFirstVideoEditor-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  myFirstVideoEditor-Bridging-Header.m
+//  myFirstVideoEdit
+//
+//  Created by Guido Grassel on 25/09/14.
+//  Copyright (c) 2014 Guido Grassel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
