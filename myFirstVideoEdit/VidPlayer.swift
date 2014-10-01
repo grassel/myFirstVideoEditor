f1e4c9cb1f7dd495afb98797b489219f28f4d662
Guido Grassel <Guido.grassel@nokia.com>
2014-10-01 12:50:41 +0300

    forked out separate VidPlayer

--- myFirstVideoEdit/VidPlayer.swift
+++ myFirstVideoEdit/VidPlayer.swift
@@ -1,13 +1,124 @@
-//
-//  VidPlayer.swift
-//  myFirstVideoEdit
-//
-//  Created by Guido Grassel on 01/10/14.
-//  Copyright (c) 2014 Guido Grassel. All rights reserved.
-//
 
 import UIKit
+import Photos
+import CoreMedia;
+import QuartzCore;
 
 class VidPlayer: NSObject {
-   
+    
+    // model of all user's videos
+    var cameraModel : CameraRollModel!;
+    
+    // index of the PHAsset in cameraModel this player is playing.
+    var playingModelIndex : Int!
+    
+    var avPlayerItem : AVPlayerItem!;
+    var avPlayer : AVPlayer!
+    
+    // the UIView that is the parent to this player
+    var parentView : UIView
+    
+    // the QuarzCore layer containing this player, sublayer of parentView.layer
+    private var playerLayer : AVPlayerLayer!
+    
+    // the return value of addBoundaryTimeObserverForTimes:queue:usingBlock:
+    var avPlayerTimeObserverId : AnyObject!
+
+    init(parentView : UIView, cameraModel : CameraRollModel){
+        self.parentView = parentView;
+        self.cameraModel = cameraModel;
+        super.init()
+        self.instalAVPlayerToViewHierarchie();
+    }
+    
+    deinit {
+        self.deinstalAVPlayerFromViewHierarchie();
+    }
+    
+    private func instalAVPlayerToViewHierarchie() {
+        // see https://developer.apple.com/LIBRARY/ios/documentation/AVFoundation/Reference/AVPlayerLayer_Class/index.html#//apple_ref/occ/cl/AVPlayerLayer
+        self.avPlayer = AVPlayer();
+        var parentLayer : CALayer = self.parentView.layer;
+        parentLayer.backgroundColor =  UIColor.orangeColor().CGColor
+        
+        self.playerLayer = AVPlayerLayer(player: self.avPlayer)
+        playerLayer.frame.origin = CGPoint(x: 0, y: 0)
+        playerLayer.frame.size = CGSize(width: parentLayer.frame.width, height: parentLayer.frame.height)
+        
+        playerLayer.backgroundColor = UIColor.blueColor().CGColor
+        
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+        parentLayer.addSublayer(playerLayer)
+    }
+    
+    private func deinstalAVPlayerFromViewHierarchie() {
+        self.playerLayer.removeFromSuperlayer();
+    }
+    
+    func deinitAVPlayer() {
+        if (self.avPlayer == nil) {
+            return;
+        }
+        self.avPlayer.pause()
+        
+        // unregister for notifications
+        var notifCtr = NSNotificationCenter.defaultCenter();
+        if (self.avPlayerItem != nil) {
+            notifCtr.removeObserver(self.avPlayerItem)
+        }
+        self.avPlayer.removeTimeObserver(self.avPlayerTimeObserverId)
+    }
+    
+    func playAsset(assetIndex : Int) {
+        self.playingModelIndex = assetIndex;
+        
+        if (playingModelIndex == nil || cameraModel == nil) {
+            println("prepareToPlayAsset: playingModelIndex or cameeraModel unset. Bailing out");
+            return;
+        }
+        
+        cameraModel!.fetchAssetFullAsync(playingModelIndex!, handler: { (indexBack : Int, avPlayerItemBack : AVPlayerItem!) -> Void in
+            if (avPlayerItemBack == nil) {
+                println("prepareToPlayAsset: failed to fetch playerItem Bailing out");
+                return;
+            }
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setupAndPlayItem(avPlayerItemBack)
+            });
+        });
+    }
+    
+    private func setupAndPlayItem(item : AVPlayerItem!) {
+        if (item == nil) {
+            println("setupAndPlayerItem item is nil. Ignoring");
+            return;
+        }
+        self.avPlayerItem = item!;
+        self.avPlayer!.replaceCurrentItemWithPlayerItem(self.avPlayerItem)
+        
+        var notifCtr = NSNotificationCenter.defaultCenter();
+        notifCtr.addObserverForName(AVPlayerItemDidPlayToEndTimeNotification, object: self.avPlayerItem, queue: NSOperationQueue.mainQueue(), usingBlock: self.playerItemDidReachEnd)
+        
+        self.avPlayer!.seekToTime(kCMTimeZero);
+        self.avPlayerTimeObserverId = self.avPlayer.addPeriodicTimeObserverForInterval(CMTimeMake(5, 25) /* 30fps */, queue: nil, usingBlock: self.playerTimeUpdate)
+        
+        self.avPlayer!.play()
+    }
+    
+    private func playerTimeUpdate(time : CMTime) {
+        println("playerTimeUpdate \(CMTimeGetSeconds(time))");
+    }
+    
+    internal func playerItemDidReachEnd(notification : NSNotification!) {
+        println("playerItemDidReachEnd");
+    }
+    
+    // we do not use the observer mechanism, method is not used
+    internal override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafeMutablePointer<Void>) {
+        if (keyPath != nil && object != nil) {
+            println("observeValueForKeyPath: \(keyPath!), \(object!.description)");
+        }
+        
+        super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context);
+    }
 }
--- myFirstVideoEdit/VidPlayerViewController.swift
+++ myFirstVideoEdit/VidPlayerViewController.swift
@@ -15,13 +15,8 @@ class VidPlayerViewController: UIViewController {
 
     var playingModelIndex : Int!
     var cameraModel : CameraRollModel!
-    
-    var avPlayerItem : AVPlayerItem!;
-    var avPlayer : AVPlayer!
-    
-    // the return value of addBoundaryTimeObserverForTimes:queue:usingBlock:
-    var avPlayerTimeObserverId : AnyObject!
-    
+    var vidPlayer : VidPlayer!;
+      
     @IBOutlet weak var avPlayerContainingView: UIView!
     
     override func viewDidLoad() {
@@ -33,95 +28,15 @@ class VidPlayerViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        instalAVPlayerToViewHierarchie();
-        playAsset();
+        self.vidPlayer = VidPlayer(parentView: avPlayerContainingView!, cameraModel: cameraModel)
+        self.vidPlayer.playAsset(playingModelIndex);
     }
     
     override func viewWillDisappear(animated: Bool) {
-        self.deinitAVPlayer();
+        self.vidPlayer.deinitAVPlayer();
         super.viewWillDisappear(animated)
     }
     
-    func instalAVPlayerToViewHierarchie() {
-        // see https://developer.apple.com/LIBRARY/ios/documentation/AVFoundation/Reference/AVPlayerLayer_Class/index.html#//apple_ref/occ/cl/AVPlayerLayer
-        self.avPlayer = AVPlayer();
-        var parentLayer : CALayer = self.avPlayerContainingView.layer;
-        parentLayer.backgroundColor =  UIColor.orangeColor().CGColor
-        
-        var playerLayer : AVPlayerLayer = AVPlayerLayer(player: self.avPlayer)
-        playerLayer.frame.origin = CGPoint(x: 0, y: 0)
-        playerLayer.frame.size = CGSize(width: parentLayer.frame.width, height: parentLayer.frame.height)
-        
-        playerLayer.backgroundColor = UIColor.blueColor().CGColor
-        
-        playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
-        parentLayer.addSublayer(playerLayer)
-    }
-    
-    func deinitAVPlayer() {
-        if (self.avPlayer == nil) {
-            return;
-        }
-        self.avPlayer.pause()
-        
-        // unregister for notifications
-        var notifCtr = NSNotificationCenter.defaultCenter();
-        if (self.avPlayerItem != nil) {
-            notifCtr.removeObserver(self.avPlayerItem)
-        }
-        self.avPlayer.removeTimeObserver(self.avPlayerTimeObserverId)
-    }
-    
-    func playAsset() {
-        if (playingModelIndex == nil || cameraModel == nil) {
-            println("prepareToPlayAsset: playingModelIndex or cameeraModel unset. Bailing out");
-            return;
-        }
-        
-        cameraModel!.fetchAssetFullAsync(playingModelIndex!, handler: { (indexBack : Int, avPlayerItemBack : AVPlayerItem!) -> Void in
-            if (avPlayerItemBack == nil) {
-                println("prepareToPlayAsset: failed to fetch playerItem Bailing out");
-                return;
-            }
-            dispatch_async(dispatch_get_main_queue(), {
-                self.setupAndPlayItem(avPlayerItemBack)
-            });
-        });
-    }
-    
-    func setupAndPlayItem(item : AVPlayerItem!) {
-        if (item == nil) {
-            println("setupAndPlayerItem item is nil. Ignoring");
-            return;
-        }
-        self.avPlayerItem = item!;
-        self.avPlayer!.replaceCurrentItemWithPlayerItem(self.avPlayerItem)
-        
-        var notifCtr = NSNotificationCenter.defaultCenter();
-        notifCtr.addObserverForName(AVPlayerItemDidPlayToEndTimeNotification, object: self.avPlayerItem, queue: NSOperationQueue.mainQueue(), usingBlock: self.playerItemDidReachEnd)
-        
-        self.avPlayer!.seekToTime(kCMTimeZero);
-        self.avPlayerTimeObserverId = self.avPlayer.addPeriodicTimeObserverForInterval(CMTimeMake(5, 25) /* 30fps */, queue: nil, usingBlock: self.playerTimeUpdate)
-        
-        self.avPlayer!.play()
-    }
-    
-    func playerTimeUpdate(time : CMTime) {
-        println("playerTimeUpdate \(CMTimeGetSeconds(time))");
-    }
-    
-    func playerItemDidReachEnd(notification : NSNotification!) {
-        println("playerItemDidReachEnd");
-    }
-    
-    // we do not use the observer mechanism, method is not used
-    override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafeMutablePointer<Void>) {
-        if (keyPath != nil && object != nil) {
-            println("observeValueForKeyPath: \(keyPath!), \(object!.description)");
-        }
-        
-        super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context);
-    }
 
    @IBAction func cancelSelected(sender: AnyObject) {
         // got back to the list of my videos
